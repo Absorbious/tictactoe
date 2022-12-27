ccc33089ae830a3349d1f3ac4a2e3298e67d8069
Absorbious <adnanjusic98@gmail.com>
2022-12-27 15:43:56 +0100

    Pseudo code written

---
+++ app.js
@@ -0,0 +1,10 @@
+// User comes to the page and chooses the character - "X" or "O"
+// By default the User is "X" 
+// User clicks on the box of choice and adds a character to the box
+// AI adds the other character to one of the empty boxes
+// If one gets 3 boxes in a row, he wins
+
+// Add Event Listeners for every box in the grid
+// When event fires off, check if the box is empty, if true then add character, else do nothing
+// Check if there are 3 in a row, declare winner
+// Reset game button resets the boxes to empty
